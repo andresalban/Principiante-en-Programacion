b92416d505b1d09d9e4d41676fc2dd6e92816156
Andrés Albán <andresalban@unicauca.edu.co>
2024-01-19 10:19:52 -0500

    se incluyen console.log
    se incluyen comandos console.log en diferentes partes del código para verificar el flujo del programa

--- 1.Lógica de programación sumérgete en la programación con JavaScript/2.Condicionales y concatenación/2034-logica-programacion-1-Aula2/app.js
+++ 1.Lógica de programación sumérgete en la programación con JavaScript/2.Condicionales y concatenación/2034-logica-programacion-1-Aula2/app.js
@@ -1,16 +1,19 @@
-//Variables
-let numeroSecreto = 2;
-let numeroUsuario = prompt("Me indicas un número entre 1 y 10 por favor:");
+alert('Bienvenido al juego del número secreto');
 
-console.log(numeroUsuario);
-/*
-Este código realiza
-la comparación
-*/
-if (numeroUsuario == numeroSecreto) {
-    //Acertamos, fue verdadera la condición
-    alert(`Acertaste, el número es: ${numeroUsuario}`);
+// Agrega un console.log para verificar el valor de "intento" después de la entrada del usuario
+let intento = prompt('Elige un número entre 1 y 10');
+console.log('Valor de intento:', intento);
+
+let numeroSecreto = 4;
+
+// Agrega un console.log para verificar la comparación entre "intento" y "numeroSecreto"
+console.log('Resultado de la comparación:', intento == numeroSecreto);
+
+if (intento == numeroSecreto) {
+    alert('Adivinaste');
 } else {
-    //La condición no se cumplió
-    alert('Lo siento, no acertaste el número');
+    // Agrega un console.log para verificar el valor de "numeroSecreto" cuando el jugador se equivoca
+    console.log('Valor del número secreto:', numeroSecreto);
+    alert('El número secreto era ' + numeroSecreto);
 }
+
