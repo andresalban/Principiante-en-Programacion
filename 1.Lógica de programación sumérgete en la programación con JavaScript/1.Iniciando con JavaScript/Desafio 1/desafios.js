fee47dc05dc9fbcfc2ce264b49153db6ab7b0fc9
Andrés Albán <andresalban@unicauca.edu.co>
2024-01-19 00:30:27 -0500

    Desafios terminados
    Se crea un archivo index.html
    desafios.js contiene la lógica de los desafíos

---
+++ 1.Lógica de programación sumérgete en la programación con JavaScript/1.Iniciando con JavaScript/Desafio 1/desafios.js
@@ -0,0 +1,30 @@
+//Desafio 1
+alert("¡Bienvenida y bievenido a nuestro sitio web!");
+
+//Desafio 2
+let nombre="luna";
+
+//Desafio 3
+let edad = 25;
+
+//Desafio 4
+let numeroDeVentas =50;
+
+//Desafio 5
+let saldoDisponible  =100;
+
+//Desafio 6
+alert("¡Error! Completa todos los campos");
+
+//Desafio 7
+let mensajeDeError ="¡Error! Completa todos los campos";
+alert(mensajeDeError);
+
+//Desafio 8
+nombre=prompt("Cual es tu nombre");
+
+//Desafio 9
+edad=prompt("Cual es tu edad");
+if (edad>=18) {
+    alert("Puedes obtener tu licencia de conducir!")
+}
