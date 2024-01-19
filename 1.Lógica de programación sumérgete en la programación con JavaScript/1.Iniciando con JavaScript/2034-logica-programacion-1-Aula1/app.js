8492d27370a29b8aa71f192fe5874f81aa84f4f4
Andrés Albán <andresalban@unicauca.edu.co>
2024-01-18 23:54:10 -0500

    Initial commit

---
+++ 1.Lógica de programación sumérgete en la programación con JavaScript/1.Iniciando con JavaScript/2034-logica-programacion-1-Aula1/app.js
@@ -0,0 +1,13 @@
+let mensajeDeBienvenida = 'Bienvenido a la aplicación';
+alert('mensajeDeBienvenida');
+
+
+let numeroSecreto=6;
+let numeroUsuario= prompt("Me indicas un numero por favor");
+
+if (numeroUsuario==numeroSecreto) {
+    alert("Acertartes el numeroo");
+}
+
+
+
