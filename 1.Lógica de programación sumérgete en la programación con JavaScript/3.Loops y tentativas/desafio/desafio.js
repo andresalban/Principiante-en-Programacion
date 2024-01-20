300d502837143df7082d0a772b403f1dcde4ef8d
Andrés Albán <andresalban@unicauca.edu.co>
2024-01-20 18:36:07 -0500

    Proyecto aula anterior

--- 1.Lógica de programación sumérgete en la programación con JavaScript/3.Loops y tentativas/desafio/desafio.js
+++ 1.Lógica de programación sumérgete en la programación con JavaScript/3.Loops y tentativas/desafio/desafio.js
@@ -1,35 +1,35 @@
 alert("Para ver los resultados abrir la consola del navegador")
 //Desafio 1
 console.log("Desafio1");
-let contador1 =1;
-while (contador1<=10) {
+let contador1 = 1;
+while (contador1 <= 10) {
     console.log(contador1)
-    contador1 ++
+    contador1++
 }
 
 //Desafio 2
 console.log("Desafio 2 ");
-let contador2 =10;
-while (contador2>0) {
+let contador2 = 10;
+while (contador2 > 0) {
     console.log(contador2);
     contador2--;
 }
 
 //Desafio 3
 console.log("Desafio 3 ");
-let numeroUsuario1 =prompt("Por favor ingrese un numero");
-while (numeroUsuario1>= 0) {
+let numeroUsuario1 = prompt("Por favor ingrese un numero");
+while (numeroUsuario1 >= 0) {
     console.log(numeroUsuario1);
-    numeroUsuario1  -- 
-    
+    numeroUsuario1--
+
 }
 
 //Desafio 4
 console.log("Desafio 4 ");
-let numeroUsuario2 =prompt("Por favor ingrese un numero");
-let contador4=0
+let numeroUsuario2 = prompt("Por favor ingrese un numero");
+let contador4 = 0
 while (contador4 <= numeroUsuario2) {
     console.log(contador4);
-    contador4++;    
-    
+    contador4++;
+
 }
