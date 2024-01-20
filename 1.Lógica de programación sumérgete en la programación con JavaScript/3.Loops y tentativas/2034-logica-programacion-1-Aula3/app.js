2d2770e12a6826cd6daf3f624c91791357aa9d8b
Andrés Albán <andresalban@unicauca.edu.co>
2024-01-20 12:47:49 -0500

    Desafio a desarrollar

--- 1.Lógica de programación sumérgete en la programación con JavaScript/3.Loops y tentativas/2034-logica-programacion-1-Aula3/app.js
+++ 1.Lógica de programación sumérgete en la programación con JavaScript/3.Loops y tentativas/2034-logica-programacion-1-Aula3/app.js
@@ -2,7 +2,7 @@
 let numeroSecreto = 4;
 let numeroUsuario = 0;
 let intentos = 1;
-let palabraVeces = 'vez';
+let palabraVeces = "vez";
 
 while (numeroUsuario != numeroSecreto) {
     numeroUsuario = prompt("Me indicas un número entre 1 y 10 por favor:");
@@ -10,16 +10,18 @@ while (numeroUsuario != numeroSecreto) {
     console.log(numeroUsuario);
     if (numeroUsuario == numeroSecreto) {
         //Acertamos, fue verdadera la condición
-        alert(`Acertaste, el número es: ${numeroUsuario}. Lo hiciste en ${intentos} ${palabraVeces}`);
+        alert(
+            `Acertaste, el número es: ${numeroUsuario}. Lo hiciste en ${intentos} ${palabraVeces}`
+        );
     } else {
         if (numeroUsuario > numeroSecreto) {
-            alert('El número secreto es menor');
+            alert("El número secreto es menor");
         } else {
-            alert('El número secreto es mayor');
+            alert("El número secreto es mayor");
         }
         //Incrementamos el contador cuando no acierta
         intentos = intentos + 1;
-        palabraVeces = 'veces';
+        palabraVeces = "veces";
         //La condición no se cumplió
         //alert('Lo siento, no acertaste el número');
     }
---
+++ 1.Lógica de programación sumérgete en la programación con JavaScript/3.Loops y tentativas/desafio/README.md
@@ -0,0 +1,6 @@
+# Desafios 
+
+1. Crea un contador que comience en 1 y vaya hasta 10 usando un bucle 'while'. Muestra cada número.
+2. Crea un contador que comience en 10 y vaya hasta 0 usando un bucle 'while'. Muestra cada número.
+3. Crea un programa de cuenta progresiva. Pide un número y cuenta desde 0 hasta ese número utilizando un bucle 'while' en la consola del navegador.
+4. Crea un programa de cuenta progresiva. Pide un número y cuenta desde 0 hasta ese número utilizando un bucle 'while' en la consola del navegador.
