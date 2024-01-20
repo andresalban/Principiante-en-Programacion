7d563c8a2604e5ece1e878170b08a0b2b49413f3
Andrés Albán <andresalban@unicauca.edu.co>
2024-01-20 14:17:48 -0500

    Desafio aula 3 terminado

---
+++ 1.Lógica de programación sumérgete en la programación con JavaScript/3.Loops y tentativas/desafio/desafio.js
@@ -0,0 +1,23 @@
+//Desafio 1
+console.log("Desafio1");
+let contador1 =1;
+while (contador1<=10) {
+    console.log(contador1)
+    contador1 = contador1+1
+}
+
+//Desafio 2
+console.log("Desafio 2 ");
+let contador2 =10;
+while (contador2>0) {
+    console.log(contador2);
+    contador2 = contador2-1;
+}
+
+//Desafio 3
+let numeroUsuario =prompt("Por favor ingrese un numero");
+let contador3=0
+while (contador3 <= numeroUsuario) {
+    console.log(contador3);
+    contador3=contador3+1;    
+}
