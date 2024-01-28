35d68d3868f289e89706880bc11247e72abdf99b
Andrés Albán <andresalban@unicauca.edu.co>
2024-01-28 12:36:35 -0500

    Archivo base desafío 1

---
+++ 2.Lógica de programación explorar funciones y listas/1. Interactuando con HTML/2035-logica-programacion-2-Aula1/app.js
@@ -0,0 +1,10 @@
+let titulo =document.querySelector('h1');
+titulo.innerHTML = 'Juego del numero secreto';
+
+let parrafo= document.querySelector('p');
+parrafo.innerHTML = 'Indica un numero del 1 al 10';
+
+function intentoUsuario() {
+    
+    
+}
