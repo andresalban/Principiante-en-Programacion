d9907753924611b0de782d29e99b7183f246085b
Andrés Albán <andresalban@unicauca.edu.co>
2024-01-30 20:10:30 -0500

    desafío aula 2 terminado

---
+++ 2.Lógica de programación explorar funciones y listas/2. Funciones/2035-logica-programacion-2-Aula2/Desafio/desafio.js
@@ -0,0 +1,35 @@
+// Desafio 1
+function saludoConsola() {
+    console.log("Hola, mundo!");
+    }
+saludoConsola();
+
+// Desafio 2
+function saludoNombre(nombre) {
+    console.log(`Hola, ${nombre}`);    
+}
+saludoNombre('Andres');
+
+// Desafio 3
+function doblarNumero(numero){
+    return numero*2;
+}
+console.log(doblarNumero(10));
+
+// Desafio 4
+function promedio(n1,n2,n3) {
+    return (n1+ n2+ n3)/3;
+}
+console.log(promedio(10,10,10));
+
+// Desafio 5
+function numeroMayor(n1,n2) {
+    return n1>n2?n1:n2;
+}
+console.log(numeroMayor(10,12));
+
+//desafio 6
+function cuadrado(n1) {
+    return n1*n1;
+}
+console.log(cuadrado(10));
